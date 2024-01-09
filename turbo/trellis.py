65b88ede679879454641170a3a013d3d73f08c20
davidsgonza <69129653+davidsgonza@users.noreply.github.com>
2024-01-08 23:17:35 -0500

    Update trellis.py
    tuples

--- turbo/trellis.py
+++ turbo/trellis.py
@@ -14,10 +14,14 @@ class Trellis:
 
     def __init__(self):
         self.transition_matrix = np.array(
-                                  [[(-1, -1), None, (1, 1), None],
-                                  [(1, -1), None, (-1, 1), None],
-                                  [None, (-1, -1), None, (1, 1)],
-                                  [None, (1, -1), None, (-1, 1)]])
+            [[(-1, -1), (0, 0), (1, 1), (0, 0)],
+             [(1, -1), (0, 0), (-1, 1), (0, 0)],
+             [(0, 0), (-1, -1), (0, 0), (1, 1)],
+             [(0, 0), (1, -1), (0, 0), (-1, 1)]])
+
+        # Convertir los elementos con tuplas a tuplas con valores de 0
+        self.transition_matrix = np.array([[tuple(0 if el == (0, 0) else el) for el in row] for row in self.transition_matrix])
+
 
         self.past_states = [(0, 1), (2, 3), (0, 1), (2, 3)]
         self.future_states = [(0, 2), (0, 2), (1, 3), (1, 3)]
